dcef5e420aa8da165e2aef55c24cb0c15398f50c
Ciprian Roman <ciprian@dreamitget.it>
2016-11-12 20:13:01 +0000

    Implemented 'products' module functionality.

--- src/products/products.js
+++ src/products/products.js
@@ -1,23 +1,43 @@
 "use strict";
 
+let Product = require("./product").Product;
+
 module.exports = {
   Products: Products
 };
 
-function Products() {}
+function Products() {
+  this._products = {};
+}
 
 Products.prototype.add = function (product) {
-  
+  if (!(product instanceof Product)) {
+    throw new Error("The 'product' parameter has an invalid type");
+  }
+  this._products[product.getId()] = product;
+
+  return this;
 };
 
 Products.prototype.get = function (productId) {
-  
+  return this._products[productId];
 };
 
-Products.prototype.update = function (product) {
-  
+Products.prototype.update = function (productId, newProduct) {
+  if (!(newProduct instanceof Product)) {
+    throw new Error("The 'newProduct' parameter has an invalid type");
+  }
+  if (productId !== newProduct.getId()) {
+    throw new Error("The product ids do not match");
+  }
+
+  this._products[productId] = newProduct;
+
+  return this;
 };
 
 Products.prototype.remove = function (productId) {
-  
+  delete this._products[productId];
+
+  return this;
 };
--- test/products/products_test.js
+++ test/products/products_test.js
@@ -7,15 +7,30 @@ let products = require("../../src/products");
 describe("Products", () => {
 
   it("should throw an error for invalid 'add' parameters", () => {
-    var fn = function (product) {
+    let fn = function (product) {
       let productsDb = new products.Products();
       productsDb.add(product);
+    };
+
+    expect(fn).to.throw(/invalid/);
+    expect(fn.bind(null, "")).to.throw(/invalid/);
+    expect(fn.bind(null, {})).to.throw(/invalid/);
+    expect(fn.bind(null, new Array())).to.throw(/invalid/);
+  });
+
+  it("should throw an error for invalid 'update' parameters", () => {
+    let fn = function (product, productId) {
+      let productsDb = new products.Products();
+      productsDb.update(productId, product);
     };
 
     expect(fn.bind(null, undefined)).to.throw(/invalid/);
     expect(fn.bind(null, "")).to.throw(/invalid/);
     expect(fn.bind(null, {})).to.throw(/invalid/);
     expect(fn.bind(null, new Array())).to.throw(/invalid/);
+
+    let butter = new products.Product({id: 1, name: "Butter", price: 0.80});
+    expect(fn.bind(null, butter, 2)).to.throw(/not match/);
   });
 
   it("should return the correct added product", () => {
@@ -62,5 +77,12 @@ describe("Products", () => {
 
     expect(result).to.equal(productsDb);
     expect(productsDb.get(butter.getId())).to.be.undefined;
+  });
+
+  it("should not fail when removing a missing product", () => {
+    let productsDb = new products.Products();
+    let result = productsDb.remove(1);
+
+    expect(result).to.equal(productsDb);
   });
 });
