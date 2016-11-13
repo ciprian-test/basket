9b0a7cc966061aa2fc72d9c8f75f7f877548c7bb
Ciprian Roman <ciprian@dreamitget.it>
2016-11-13 17:47:14 +0000

    Added basket functionality.

--- src/basket.js
+++ src/basket.js
@@ -1,26 +1,54 @@
 "use strict";
 
+let utils = require("./utils");
+
 module.exports = {
   Basket: Basket
 };
 
-function Basket(products, offers) {
-  this._products = products;
-  this._offers = offers;
+function Basket(productsDb, offersDb) {
+  this._productsDb = productsDb;
+  this._offersDb = offersDb;
+  this._products = {};
 }
 
 Basket.prototype.add = function (productId, quantity) {
-  
+  let product = this._productsDb.get(productId);
+  if (!product) {
+    throw new Error("The product is missing");
+  }
+
+  this._products[productId] = (this._products[productId] || 0) + quantity;
+
+  return this;
 };
 
 Basket.prototype.update = function (productId, newQuantity) {
-  
+  let product = this._productsDb.get(productId);
+  if (!product) {
+    throw new Error("The product is missing");
+  }
+
+  this._products[productId] = newQuantity;
+
+  return this;
 };
 
 Basket.prototype.remove = function (productId) {
-  
+  delete this._products[productId];
+
+  return this;
 };
 
 Basket.prototype.getTotalCost = function () {
-  
+  let cost = 0;
+  for (let productId in this._products) {
+    if (this._products.hasOwnProperty(productId)) {
+      cost += this._productsDb.get(productId).getPrice() * this._products[productId];
+    }
+  }
+
+  let discount = this._offersDb.calculateBasketDiscount(this._products);
+
+  return utils.round(cost - discount);
 };
--- test/basket_test.js
+++ test/basket_test.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let expect = require("chai").expect;
 
 let products = require("../src/products");
@@ -5,20 +7,19 @@ let offers = require("../src/offers");
 let basket = require("../src/basket");
 
 describe("Basket", () => {
-
   let butter = new products.Product({id: 1, name: "Butter", price: 0.80});
   let milk = new products.Product({id: 2, name: "Milk", price: 1.15});
   let bread = new products.Product({id: 3, name: "Bread", price: 1.00});
 
   let productsDb = new products.Products();
-  productDb.add(butter).add(milk).add(bread);
+  productsDb.add(butter).add(milk).add(bread);
 
-  let halfOffToBread = new offer.Offer();
+  let halfOffToBread = new offers.Offer(productsDb, 1);
   halfOffToBread.include(butter.getId(), 2)
                 .include(bread.getId(), 1)
                 .setPercentageDiscountToProduct(bread.getId(), 0.5);
 
-  let forthMilkFree = new offer.Offer();
+  let forthMilkFree = new offers.Offer(productsDb, 2);
   forthMilkFree.include(milk.getId(), 4)
                .setPercentageDiscountToProduct(milk.getId(), 1.0);
 
@@ -26,56 +27,56 @@ describe("Basket", () => {
   offersDb.add(halfOffToBread).add(forthMilkFree);
 
   it("should correctly get the basket cost for an empty basket", () => {
-    let basket = new basket.Basket(productsDb, offersDb);
-    expect(basket.getTotalCost()).to.equal(0.0);
+    let customerBasket = new basket.Basket(productsDb, offersDb);
+    expect(customerBasket.getTotalCost()).to.equal(0.0);
   });
 
   it("should correctly get the basket cost for single product basket", () => {
-    let basket = new basket.Basket(productsDb, offersDb);
-    basket.add(butter.getId(), 1);
-    expect(basket.getTotalCost()).to.equal(0.8);
+    let customerBasket = new basket.Basket(productsDb, offersDb);
+    customerBasket.add(butter.getId(), 1);
+    expect(customerBasket.getTotalCost()).to.equal(0.8);
 
-    basket.remove(butter.getId()).add(milk.getId(), 1);
-    expect(basket.getTotalCost()).to.equal(1.15);
+    customerBasket.remove(butter.getId()).add(milk.getId(), 1);
+    expect(customerBasket.getTotalCost()).to.equal(1.15);
 
-    basket.remove(milk.getId()).add(bread.getId(), 1);
-    expect(basket.getTotalCost()).to.equal(1);
+    customerBasket.remove(milk.getId()).add(bread.getId(), 1);
+    expect(customerBasket.getTotalCost()).to.equal(1);
   });
 
   it("should correctly get the basket cost for basket with each product once", () => {
-    let basket = new basket.Basket(productsDb, offersDb);
-    basket.add(butter.getId(), 1)
-          .add(milk.getId(), 1)
-          .add(bread.getId(), 1);
-    expect(basket.getTotalCost()).to.equal(2.95);
+    let customerBasket = new basket.Basket(productsDb, offersDb);
+    customerBasket.add(butter.getId(), 1)
+                  .add(milk.getId(), 1)
+                  .add(bread.getId(), 1);
+    expect(customerBasket.getTotalCost()).to.equal(2.95);
   });
 
   it("should correctly get the basket cost for basket with each product multiple times", () => {
-    let basket = new basket.Basket(productsDb, offersDb);
-    basket.add(butter.getId(), 1)
-          .add(milk.getId(), 2)
-          .add(bread.getId(), 3);
-    expect(basket.getTotalCost()).to.equal(6.1);
+    let customerBasket = new basket.Basket(productsDb, offersDb);
+    customerBasket.add(butter.getId(), 1)
+                  .add(milk.getId(), 2)
+                  .add(bread.getId(), 3);
+    expect(customerBasket.getTotalCost()).to.equal(6.1);
   });
 
   it("should correctly apply the 'half off to bread' offer", () => {
-    let basket = new basket.Basket(productsDb, offersDb);
-    basket.add(butter.getId(), 2)
-          .add(bread.getId(), 1);
-    expect(basket.getTotalCost()).to.equal(3.1);
+    let customerBasket = new basket.Basket(productsDb, offersDb);
+    customerBasket.add(butter.getId(), 2)
+                  .add(bread.getId(), 2);
+    expect(customerBasket.getTotalCost()).to.equal(3.1);
   });
 
   it("should correctly apply the 'forth milk free' offer", () => {
-    let basket = new basket.Basket(productsDb, offersDb);
-    basket.add(milk.getId(), 4);
-    expect(basket.getTotalCost()).to.equal(3.45);
+    let customerBasket = new basket.Basket(productsDb, offersDb);
+    customerBasket.add(milk.getId(), 4);
+    expect(customerBasket.getTotalCost()).to.equal(3.45);
   });
 
   it("should correctly apply multiple offers", () => {
-    let basket = new basket.Basket(productsDb, offersDb);
-    basket.add(butter.getId(), 2)
-          .add(milk.getId(), 8)
-          .add(bread.getId(), 1);
-    expect(basket.getTotalCost()).to.equal(9.0);
+    let customerBasket = new basket.Basket(productsDb, offersDb);
+    customerBasket.add(butter.getId(), 2)
+                  .add(milk.getId(), 8)
+                  .add(bread.getId(), 1);
+    expect(customerBasket.getTotalCost()).to.equal(9.0);
   });
 });
